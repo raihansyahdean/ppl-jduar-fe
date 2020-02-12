6a336ca1e3a5771cd5ca922373d4e9f4aafec6da
Gusti Ngurah Yama Adi Putra <gustingurah.yama@gmail.com>
2020-02-12 15:43:12 +0700

    [CHORES] delete react logo in homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Hello World!</code><br></br><br></br>
+          <code>Hello World!</code>
+          <br></br><br></br>
           We'll come very soon...
         </p>
       </header>
