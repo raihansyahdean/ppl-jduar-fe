2c58433f71a5fe166dc6e1d6b0e8fe3fed5b0049
rafiftaris <rafif@alterra.id>
2020-02-17 21:33:30 +0700

    [CHORES] implement jest (unit test system)

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+//   moduleNameMapper: {
+//     '^@/(.*)$': '<rootDir>/$1',
+//     '^~/(.*)$': '<rootDir>/$1',
+//     '^vue$': 'vue/dist/vue.common.js'
+//   },
+
+  moduleFileExtensions: ['js', 'vue', 'json'],
+
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
+  },
+
+  collectCoverage: true,
+
+  collectCoverageFrom: [
+      '<rootDir>/src/*.vue'
+    // '<rootDir>/components/**/*.vue',
+    // '<rootDir>/pages/**/*.vue'
+  ],
+
+  preset: '@vue/cli-plugin-unit-jest'
+}
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // server.js
 
 var express = require('express');
-var path = require('path');
+// var path = require('path');
 var serveStatic = require('serve-static');
 
-app = express();
+var app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
 var port = process.env.PORT || 5000;
