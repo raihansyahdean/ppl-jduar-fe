c74d040ea465f7478cf9ccacb08f87fe75447388
Gusti Ngurah Yama Adi Putra <gustingurah.yama@gmail.com>
2020-02-25 07:24:33 +0700

    [CHORES] modified existing test

--- tests/unit/HelloWorld.spec.js
+++ tests/unit/HelloWorld.spec.js
@@ -2,16 +2,8 @@ import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
-  const wrapper = shallowMount(HelloWorld, {
-    propsData: {
-      msg: "Hello World",
-    }
-  })
-  it('renders props.msg when passed', () => {
-    const msg = 'Hello World'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
+  const wrapper = shallowMount(HelloWorld);
+  it('it contains code with love', () => {
+    expect(wrapper.html()).toContain('<p>code with ❤ by JDUAR NVIDIA</p>')
+  });
 })
