38ef88b0f3cd57e87a2d1834b1c75766a9421899
Gusti Ngurah Yama Adi Putra <gustingurah.yama@gmail.com>
2020-02-26 15:55:38 +0700

    [CHORES] minor bugs when import logo in html and make router

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>Hello World!</h1>
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png">
     <p>
       We'll come very soon...
     </p>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-import routes from './routes'
+import VueRouter from "vue-router"
+import Routes from "./routes"
 
 Vue.config.productionTip = false
-// Install Routing
+Vue.config.productionTip = false;
 Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: Routes
+});
 
 new Vue({
   render: h => h(App),
+  router: router
 }).$mount('#app')
