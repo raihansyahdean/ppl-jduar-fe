b4f939419215959fa8cee0842b87fa2aab5babe2
rafiftaris <rafif@alterra.id>
2020-02-26 18:25:08 +0700

    [CHORES] add linter stage

--- tests/unit/HelloWorld.spec.js
+++ tests/unit/HelloWorld.spec.js
@@ -2,16 +2,13 @@ import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
+  var msg = "Hello World"
   const wrapper = shallowMount(HelloWorld, {
     propsData: {
-      msg: "Hello World",
+      msg: msg,
     }
   })
   it('renders props.msg when passed', () => {
-    const msg = 'Hello World'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
     expect(wrapper.text()).toMatch(msg)
   })
 })
