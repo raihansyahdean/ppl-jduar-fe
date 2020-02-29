8429efeaee2e966396e13d55423debb1f49e0d89
Raihansyah Attallah Andrian <raihansyah.dean@gmail.com>
2020-03-01 01:15:03 +0700

    [CHORES] retry pipeline for coverage

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,10 @@
 		<router-view></router-view>
 	</div>
 </template>
-import HelloWorld from './components/HelloWorld.vue'
-import ReadyPage from './components/ReadyPage.vue'
-import Camera from './components/Camera.vue'
 
 <script>
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-	ReadyPage,
-	Camera
-  }
 }
 </script>
 
