b8a15c3a67ab18a0c9d499b53e358f464a1e8fa3
Julia Ningrum <julia.ningrum@ui.ac.id>
2020-02-29 23:30:21 +0700

    [GREEN] Implement Instruction Page

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import HelloWorld from './components/HelloWorld.vue'
 import ReadyPage from './components/ReadyPage.vue'
+import InstructionPage from './components/InstructionPage.vue'
 
 const routes = [
     {path: '/', component: HelloWorld},
-    {path: '/ready', component: ReadyPage}
+    {path: '/ready', component: ReadyPage},
+    {path: '/start', component: InstructionPage}
 ]
 
 export default routes;
