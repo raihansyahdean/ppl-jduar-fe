c18efb77e95b981115555f61da95726790eb3e61
Julia Ningrum <julia.ningrum@ui.ac.id>
2020-02-29 21:49:20 +0700

    [RED] Change tests to match new Ready Page style

--- tests/unit/ReadyPage.spec.js
+++ tests/unit/ReadyPage.spec.js
@@ -9,6 +9,6 @@ describe('ReadyPage.vue', () => {
   const wrapper = mount(ReadyPage)
 
   it('it contains ready page instruction', () => {
-    expect(wrapper.html()).toContain('Pertama-tama, posisikan kepala Anda di dalam lingkaran')
+    expect(wrapper.html()).toContain('Posisikan wajah anda di hadapan kamera')
   })
 })
