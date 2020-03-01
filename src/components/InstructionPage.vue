5dfb7da011a09416ceef07ce7084602f9401a518
Alya Zahra <zahraalya2207@gmail.com>
2020-03-01 15:41:14 +0700

    [GREEN] Implement camera on instruction page

---
+++ src/components/InstructionPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="camera">
+        <div class="mx-auto d-block" id="circle">
+            <camera/>
+        </div>
+    </div>
+</template>
+
+<script>
+import Camera from '@/components/Camera.vue';
+
+export default {
+    name: 'InstructionPage',
+    data () {
+        return {}
+    },
+    components: {
+        Camera
+    },
+};
+</script>
+
+<style>
+#circle {
+    background-color: #FFFFFF;
+    height: 230px;
+    width: 230px;
+    border-radius: 50%;
+    margin-top: 80px;
+    position: relative;
+    overflow: hidden;
+    background-size: cover;
+}
+</style>
