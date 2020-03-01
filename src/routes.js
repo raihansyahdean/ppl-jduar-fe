cfc24ba8201e6c471f36e873107d3396a2d176ca
Alya Zahra <zahraalya2207@gmail.com>
2020-03-01 17:26:50 +0700

    [CHORES] Remove unnecessary HelloWorld files

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,6 @@
-import HelloWorld from './components/HelloWorld.vue'
 import ReadyPage from './components/ReadyPage.vue'
 
 const routes = [
-    {path: '/', component: HelloWorld},
     {path: '/ready', component: ReadyPage}
 ]
 
