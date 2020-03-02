a506dc9ca29c8e57563eddcbaadda947d1ce78a8
Gusti Ngurah Yama Adi Putra <gustingurah.yama@gmail.com>
2020-03-02 12:49:48 +0700

    [REFACTOR] delete unuse import

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,7 +1,5 @@
-import { mount, createLocalVue } from "@vue/test-utils"
+import { mount } from "@vue/test-utils"
 import App from "@/App.vue"
-import VueRouter from "vue-router"
-import routes from "@/routes.js"
 
 describe('App.vue', () => {
   const wrapper = mount(App, {
