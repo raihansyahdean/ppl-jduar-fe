878a7d6919c31e70b50f8847aaf86cd0ed1ac7c9
Julia Ningrum <julia.ningrum@ui.ac.id>
2020-03-02 13:34:45 +0700

    [REFACTOR] Delete unused import

--- tests/unit/InstructionPage.spec.js
+++ tests/unit/InstructionPage.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount, mount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import InstructionPage from '@/components/InstructionPage.vue'
 import Vue from 'vue'
 import { BootstrapVue } from 'bootstrap-vue'
@@ -6,15 +6,14 @@ import { BootstrapVue } from 'bootstrap-vue'
 Vue.use(BootstrapVue)
 
 describe('InstructionPage.vue', () => {
-  const wrapper = mount(InstructionPage)
-
-  it('it contains camera video tag', () => {
-    expect(wrapper.html()).toContain('<video></video>')
-  })
-  
-  it('it can change instruction if Ambil Foto button is clicked', async () => {
-	  wrapper.find('#shoot-button').trigger('click')
-	  expect(wrapper.text()).toContain('depan')
-	  //expect(wrapper.vm.instructionIdx).toBe(0)
+	const wrapper = mount(InstructionPage)
+	
+	it('it contains camera video tag', () => {
+		expect(wrapper.html()).toContain('<video></video>')
+	})
+	
+	it('it can change instruction if Ambil Foto button is clicked', async () => {
+		wrapper.find('#shoot-button').trigger('click')
+		expect(wrapper.text()).toContain('depan')
 	})
 })
