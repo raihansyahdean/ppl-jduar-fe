a0e738e7a7f9a6e1820cc37f32c7955e71e51343
Julia Ningrum <julia.ningrum@ui.ac.id>
2020-03-05 11:48:13 +0700

    PBI-3: Regis Pelanggan

--- src/components/ReadyPage.vue
+++ src/components/ReadyPage.vue
@@ -5,15 +5,15 @@
                 <div>
                     <img id="instruction-image" alt="instruction-image" src="./../assets/img/face.png">
                 </div>
-                <div id="instruction">
+                <div id="instruction-div">
                     <div><p class="font-16-px font-weight-bold">Set Up deteksi wajah</p></div>
                     <p class="font-14-px">Posisikan wajah anda di hadapan kamera. Ikuti instruksi yang diberikan.</p>
                 </div>
                 <div>
-                    <b-button class="border-0 font-16-px font-weight-bold" id="start-button">Mulai</b-button>
+                    <b-button href="/start" class="border-0 font-16-px font-weight-bold" id="start-button">Mulai</b-button>
                 </div>
             </div>
-            <div class="float-right position-relative" id="cancel">
+            <div class="px-5" id="cancel">
                 <a href="/" class="font-16-px text-decoration-none" id="cancel-link">Batal</a>
             </div>
         </div>
@@ -31,13 +31,13 @@ export default {
 
 <style>
 /* Instruction Part */
+#instruction-div {
+    margin: 0.5rem 3rem 3rem 3rem;
+}
+
 #instruction-image {
     width: 240px;
     height: 240px;
-}
-
-#instruction {
-    margin: 0.5rem 3rem 5rem 3rem;
 }
 
 /* Start Button */
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import HomePage from "./components/HomePage.vue";
 
 import ReadyPage from './components/ReadyPage.vue'
+import InstructionPage from './components/InstructionPage.vue'
 
 const routes = [
+    {path: '/ready', component: ReadyPage},
+    {path: '/start', component: InstructionPage},
     {path:'/', component: HomePage},
-    {path: '/ready', component: ReadyPage}
 ]
 
 export default routes;
