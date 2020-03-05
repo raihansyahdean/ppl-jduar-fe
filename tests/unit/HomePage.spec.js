80942f09c26573176405bfc7a2fbdbaf3dc8d235
Gusti Ngurah Yama Adi Putra <gustingurah.yama@gmail.com>
2020-03-05 11:21:46 +0700

    [CHORES] using library to testing

--- tests/unit/HomePage.spec.js
+++ tests/unit/HomePage.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils'
+import { shallowMount, mount, createLocalVue } from '@vue/test-utils'
 import HomePage from '@/components/HomePage.vue'
 import VueRouter from 'vue-router'
 
@@ -16,75 +16,34 @@ describe('HomePage.vue', () => {
         expect(wrapper.vm.$data.imageIdentify).toBe(require('@/assets/Identify Default.png'))
         expect(wrapper.vm.$data.imageRegister).toBe(require('@/assets/Register Default.png'))
     });
-    
 
-    it("Test register image mouseover and mouseleave function", async () => {
-        const mouseOverRegister = jest.fn();
-        const mouseLeaveRegister = jest.fn();
-        const mouseClickRegister = jest.fn();
-
-        const wrapper2 = shallowMount(HomePage, {
-            localVue,
-            stubs: ["b-navbar-nav", "b-nav-item", "b-collapse", "b-navbar-toggle", "b-navbar", "b-navbar-brand"],
-            methods: {
-                mouseLeaveRegister,
-                mouseOverRegister,
-                mouseClickRegister
-            }, data() {
-                return {
-                    imageRegister: require("@/assets/Register Default.png") 
-                }
-            }
-        });
-
-        await wrapper2.vm.$nextTick()
-        var imgRegister = wrapper2.find('#register-image')
+    it("Test register image", async () => {
+        var imgRegister = wrapper.find('#register-image')
         imgRegister.trigger('mouseover')
-        expect(mouseOverRegister).toBeCalled()
-        expect(wrapper2.vm.$data.imageRegister).toBe(require('@/assets/Register Hover.png'))
+        await wrapper.vm.$nextTick()
+        expect(imgRegister.attributes("src")).toBe(require('@/assets/Register Hover.png'))
 
         imgRegister.trigger('mouseleave')
-        expect(mouseLeaveRegister).toBeCalled()
-        expect(wrapper2.vm.$data.imageRegister).toBe(require('@/assets/Register Default.png'))
+        await wrapper.vm.$nextTick()
+        expect(imgRegister.attributes("src")).toBe(require('@/assets/Register Default.png'))
 
         imgRegister.trigger('mousedown')
-        expect(mouseClickRegister).toBeCalled()
-        expect(wrapper2.vm.$data.imageRegister).toBe(require('@/assets/Register Clicked.png'))
-
+        await wrapper.vm.$nextTick()
+        expect(imgRegister.attributes("src")).toBe(require('@/assets/Register Clicked.png'))
     });
 
-    it("Test identify image mouseover and mouseleave function", async () => {
-        const mouseOverIdentify = jest.fn();
-        const mouseLeaveIdentify = jest.fn();
-        const mouseClickIdentify = jest.fn();
+    it("Test identification image", async () => {
+        var imgRegister = wrapper.find('#identify-image')
+        imgRegister.trigger('mouseover')
+        await wrapper.vm.$nextTick()
+        expect(imgRegister.attributes("src")).toBe(require('@/assets/Identify Hover.png'))
 
-        const wrapper3 = shallowMount(HomePage, {
-            localVue,
-            stubs: ["b-navbar-nav", "b-nav-item", "b-collapse", "b-navbar-toggle", "b-navbar", "b-navbar-brand"],
-            methods: {
-                mouseLeaveIdentify,
-                mouseOverIdentify,
-                mouseClickIdentify
-            }, data() {
-                return {
-                    imageIdentify: require("@/assets/Identify Default.png") 
-                }
-            }
-        });
+        imgRegister.trigger('mouseleave')
+        await wrapper.vm.$nextTick()
+        expect(imgRegister.attributes("src")).toBe(require('@/assets/Identify Default.png'))
 
-        await wrapper3.vm.$nextTick()
-        var imgIdentify = wrapper3.find('#identify-image')
-        imgIdentify.trigger('mouseover')
-        expect(mouseOverIdentify).toBeCalled()
-        expect(wrapper3.vm.$data.imageIdentify).toBe(require('@/assets/Identify Hover.png'))
-
-        imgIdentify.trigger('mouseleave')
-        expect(mouseLeaveIdentify).toBeCalled()
-        expect(wrapper3.vm.$data.imageIdentify).toBe(require('@/assets/Identify Default.png'))
-
-        imgIdentify.trigger('mousedown')
-        expect(mouseClickIdentify).toBeCalled()
-        expect(wrapper3.vm.$data.imageIdentify).toBe(require('@/assets/Identify Clicked.png'))
-
+        imgRegister.trigger('mousedown')
+        await wrapper.vm.$nextTick()
+        expect(imgRegister.attributes("src")).toBe(require('@/assets/Identify Clicked.png'))
     });
 })
