858d70b847bdafcab25799db9b0c57c6b4ea3e24
Raihansyah Attallah Andrian <raihansyah.dean@gmail.com>
2020-03-05 17:00:12 +0700

    fix server

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ var serveStatic = require('serve-static')
 var enforce = require('express-sslify');
 
 var app = express();
-app.use(serveStatic(__dirname + "/dist"));
-app.use(enforce.HTTPS());
+// Serve static assets from the build files (images, etc)
+app.use(serveStatic(path.join(__dirname, '/dist')))
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 // Use a fallback for non-root routes (required for Vue router)
 //   NOTE: History fallback must be "used" before the static serving middleware!
@@ -17,11 +18,12 @@ app.use(history({
     verbose: true
 }))
 
-// Serve static assets from the build files (images, etc)
-app.use(serveStatic(path.join(__dirname, '/dist')))
-
 var port = process.env.PORT || 5000
 
-app.listen(port, () => {
-  console.log('Server started at http://localhost:5000')
-})
+// app.listen(port, () => {
+//   console.log('Server started at http://localhost:5000')
+// })
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
