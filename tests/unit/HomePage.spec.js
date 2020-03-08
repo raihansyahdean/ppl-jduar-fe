f93d0344cd3cda7fdb5fbb9f4de3e2a46e211574
Gusti Ngurah Yama Adi Putra <gustingurah.yama@gmail.com>
2020-03-08 17:41:51 +0700

    [CHORES] relocate image in HomePage

--- tests/unit/HomePage.spec.js
+++ tests/unit/HomePage.spec.js
@@ -13,37 +13,37 @@ describe('HomePage.vue', () => {
     it("Test navbar and init image", async () => {
         await wrapper.vm.$nextTick()
         expect(wrapper.html()).toContain('NavBar')
-        expect(wrapper.vm.$data.imageIdentify).toBe(require('@/assets/Identify Default.png'))
-        expect(wrapper.vm.$data.imageRegister).toBe(require('@/assets/Register Default.png'))
+        expect(wrapper.vm.$data.imageIdentify).toBe(require('@/assets/img/HomePage/Identify Default.png'))
+        expect(wrapper.vm.$data.imageRegister).toBe(require('@/assets/img/HomePage/Register Default.png'))
     });
 
     it("Test register image", async () => {
         var imgRegister = wrapper.find('#register-image')
         imgRegister.trigger('mouseover')
         await wrapper.vm.$nextTick()
-        expect(imgRegister.attributes("src")).toBe(require('@/assets/Register Hover.png'))
+        expect(imgRegister.attributes("src")).toBe(require('@/assets/img/HomePage/Register Hover.png'))
 
         imgRegister.trigger('mouseleave')
         await wrapper.vm.$nextTick()
-        expect(imgRegister.attributes("src")).toBe(require('@/assets/Register Default.png'))
+        expect(imgRegister.attributes("src")).toBe(require('@/assets/img/HomePage/Register Default.png'))
 
         imgRegister.trigger('mousedown')
         await wrapper.vm.$nextTick()
-        expect(imgRegister.attributes("src")).toBe(require('@/assets/Register Clicked.png'))
+        expect(imgRegister.attributes("src")).toBe(require('@/assets/img/HomePage/Register Clicked.png'))
     });
 
     it("Test identification image", async () => {
         var imgRegister = wrapper.find('#identify-image')
         imgRegister.trigger('mouseover')
         await wrapper.vm.$nextTick()
-        expect(imgRegister.attributes("src")).toBe(require('@/assets/Identify Hover.png'))
+        expect(imgRegister.attributes("src")).toBe(require('@/assets/img/HomePage/Identify Hover.png'))
 
         imgRegister.trigger('mouseleave')
         await wrapper.vm.$nextTick()
-        expect(imgRegister.attributes("src")).toBe(require('@/assets/Identify Default.png'))
+        expect(imgRegister.attributes("src")).toBe(require('@/assets/img/HomePage/Identify Default.png'))
 
         imgRegister.trigger('mousedown')
         await wrapper.vm.$nextTick()
-        expect(imgRegister.attributes("src")).toBe(require('@/assets/Identify Clicked.png'))
+        expect(imgRegister.attributes("src")).toBe(require('@/assets/img/HomePage/Identify Clicked.png'))
     });
 })
